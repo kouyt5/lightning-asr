8e9a1d600b46a45c69b3e92821e8d45bfad43513
kouyt5 <1147893200@qq.com>
2021-01-27 12:54:55 +0800

    语音识别迁移

---
+++ models/QuartNet.py
@@ -0,0 +1,231 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+class SeprationConv(nn.Module):
+    def __init__(self, in_ch, out_ch, k=33, last=False, mask=True, dilation=1, stride=1):
+        super(SeprationConv, self).__init__()
+        self.last = last
+        self.mask = mask
+        if dilation > 1:
+            self.depthwise_conv = nn.Conv1d(in_ch, in_ch, kernel_size=k, stride=stride,
+                                            padding=(dilation * k) // 2 - 1, groups=in_ch, dilation=dilation)
+        else:
+            self.depthwise_conv = nn.Conv1d(in_ch, in_ch, kernel_size=k, stride=stride,
+                                            padding=k // 2, groups=in_ch, dilation=dilation)
+        self.pointwise_conv = nn.Conv1d(in_ch, out_ch, kernel_size=1, stride=stride)
+        self.bn = nn.BatchNorm1d(out_ch)
+        self.relu = nn.ReLU()
+        self.maskcnn = MaskCNN()
+
+    def forward(self, input, percents):
+        x = self.depthwise_conv(input)
+        x = self.pointwise_conv(x)
+        x = self.channel_shuffle(x, groups=1)
+        if self.mask:
+            x = self.maskcnn(x, percents)
+        x = self.bn(x)
+        if not self.last:
+            x = self.relu(x)
+        return x
+
+    def channel_shuffle(self, x, groups):
+        batchsize, num_channels, time = x.data.size()
+        channels_per_group = num_channels // groups
+        if not channels_per_group * groups == num_channels:
+            raise Exception("group数和通道数不匹配，请保证group能够被num_channels整除")
+        # reshape
+        x = x.view(batchsize, groups,
+                   channels_per_group, time)
+        x = torch.transpose(x, 1, 2).contiguous()
+        # flatten
+        x = x.view(batchsize, -1, time)
+        return x
+
+
+class QuartNetBlock(nn.Module):
+    def __init__(self, repeat=3, in_ch=1, out_ch=32, k=33, mask=True):
+        super(QuartNetBlock, self).__init__()
+        seq = []
+        for i in range(0, repeat - 1):
+            sep = SeprationConv(in_ch, in_ch, k, mask)
+            seq.append(sep)
+        self.reside = nn.Sequential(
+            nn.Conv1d(in_ch, out_ch, kernel_size=1),
+            nn.BatchNorm1d(out_ch),
+        )
+        last_sep = SeprationConv(in_ch, out_ch, k=k, last=True, mask=mask)
+        seq.append(last_sep)
+        self.seq = nn.ModuleList(seq)
+        self.last_relu = nn.ReLU()
+
+    def forward(self, x, percents):
+        start = x
+        for m in self.seq:
+            x = m(x, percents)
+        res_out = self.reside(start)
+        x = x + res_out
+        x = self.last_relu(x)
+        return x
+
+
+class QuartNet(nn.Module):
+    def __init__(self):
+        super(QuartNet, self).__init__()
+        self.first_cnn = nn.Sequential(
+            nn.Conv1d(64, 256, kernel_size=33, stride=2,
+                      padding=16),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+        )
+        # self.first_cnn = SeprationConv(64,256,33,stride=2,mask=True)
+        self.block1 = QuartNetBlock(repeat=5, in_ch=256, out_ch=256, k=33)
+        # self.block12 = QuartNetBlock(repeat=5,in_ch=256,out_ch=256,k=33) # add layer
+        self.block2 = QuartNetBlock(repeat=5, in_ch=256, out_ch=256, k=39)
+        # self.block22 = QuartNetBlock(repeat=5,in_ch=256,out_ch=256,k=39) # add layer
+        self.block3 = QuartNetBlock(repeat=5, in_ch=256, out_ch=512, k=51)
+        self.block4 = QuartNetBlock(repeat=5, in_ch=512, out_ch=512, k=63)
+        self.block5 = QuartNetBlock(repeat=5, in_ch=512, out_ch=512, k=75)
+
+        # self.last_cnn = nn.Sequential(
+        #     nn.Conv1d(512, 512, kernel_size= 87, stride=1,groups=512,
+        #               padding=87//2),
+        #     nn.Conv1d(512, 512, kernel_size=1, stride=1),
+        #     nn.BatchNorm1d(512),
+        #     nn.ReLU(),
+        # )
+        # self.last_cnn = QuartNetBlock(repeat=1,in_ch=512,out_ch=512,k=87,mask=False)
+        self.last_cnn = SeprationConv(512, 512, k=87, last=False, mask=True, dilation=1)  # 空洞率为2收敛比1慢 cer0.99-->0.92
+        # self.last_cnn = nn.Sequential(
+        #     nn.Conv1d(512, 512, kernel_size= 87, stride=1, groups=512,dilation=1,
+        #         padding=87//2),
+        #     nn.Conv1d(512, 512, kernel_size=1, stride=1),
+        #     nn.BatchNorm1d(512),
+        #     nn.ReLU(),
+        # )
+        self.last_cnn2 = nn.Sequential(
+            nn.Conv1d(512, 1024, kernel_size=1, stride=1),
+            nn.BatchNorm1d(1024),
+            nn.ReLU(),
+        )
+
+    def forward(self, input, percents):
+        # x = input.view(input.size(0),input.size(2),input.size(3))
+        x = input.squeeze(dim=1).contiguous()
+        x = self.first_cnn(x)
+        x = self.block1(x, percents)
+        # x = self.block12(x,percents)
+        x = self.block2(x, percents)
+        # x = self.block22(x,percents)
+        x = self.block3(x, percents)
+        x = self.block4(x, percents)
+        x = self.block5(x, percents)
+        x = self.last_cnn(x, percents)
+        x = self.last_cnn2(x)
+        return x
+
+
+class BatchLSTM(nn.Module):
+    def __init__(self, in_ch=64, out_ch=512,
+                 batch_f=True, bidirection=True):
+        super().__init__()
+        self.rnn = nn.LSTM(in_ch, out_ch, num_layers=1,
+                           batch_first=batch_f, bidirectional=bidirection)
+
+    def forward(self, x, length):
+        x = nn.utils.rnn.pack_padded_sequence(x, enforce_sorted=False,
+                                              lengths=length, batch_first=True)
+        x, h = self.rnn(x)
+        # x, h = self.rnn2(x)
+        x, _ = nn.utils.rnn.pad_packed_sequence(x, batch_first=True)  # N*T*C
+        return x
+
+
+class MyModel(nn.Module):
+    def __init__(self):
+        super(MyModel, self).__init__()
+        # self.maskcnn = MaskCNN()
+        self.cnn = QuartNet()
+        self.rnn = BatchLSTM(64 * 128, 128, True, True)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.fc = nn.Linear(256, 29)
+
+    def forward(self, input, percents):
+        x = self.cnn(input, percents)  # N*32*F*T
+        # x = self.maskcnn(x, percents)
+        x = x.view(x.size(0), x.size(1) * x.size(2), -1).transpose(1, 2).contiguous()
+        lengths = torch.mul(x.size(1), percents).int()
+        x = self.rnn(x, lengths)
+        x = x.transpose(1, 2)  # N*C*T
+        x = self.bn1(x)
+        x = self.fc(x.transpose(1, 2))  # N*T*class
+        x = nn.functional.log_softmax(x, dim=-1)
+        return x
+
+
+class MyModel2(nn.Module):
+    def __init__(self, labels):
+        super(MyModel2, self).__init__()
+        # self.maskcnn = MaskCNN()
+        self.labels = labels
+        self.cnn = QuartNet()
+        self.last_cnn3 = nn.Sequential(
+            nn.Conv1d(1024, len(self.labels)+1, kernel_size=1, stride=1, dilation=1),  # 空洞率2较好 cer=0.98-->0.53(dila=1)
+            nn.BatchNorm1d(len(self.labels)+1),
+            nn.ReLU(),
+        )
+        # self.rnn = BatchLSTM(64*128,128,True,True)
+        # self.bn1 = nn.BatchNorm1d(256)
+        # self.fc = nn.Linear(256, 29)
+
+    def forward(self, input, percents):
+        x = self.cnn(input, percents)  # N*C*T
+        x = self.last_cnn3(x)
+        # x = self.maskcnn(x, percents)
+        # x = x.view(x.size(0), x.size(1)*x.size(2), -1).transpose(1, 2).contiguous()
+        # lengths=torch.mul(x.size(1), percents).int()
+        # x = self.rnn(x, lengths)
+        x = x.transpose(1, 2)  # N*T*C
+        # x = self.bn1(x)
+        # x = self.fc(x)  # N*T*class
+        x = nn.functional.log_softmax(x, dim=-1)
+        return x
+
+
+class MaskCNN1(nn.Module):
+    def forward(self, x, percents):
+        lengths = torch.mul(x.size(3), percents).int()
+        mask = torch.BoolTensor(x.size()).fill_(0)
+        if x.is_cuda:
+            mask = mask.cuda()
+        for i, length in enumerate(lengths):
+            length = length.item()
+            if (mask[i].size(2) - length) > 0:
+                mask[i].narrow(
+                    2, length, mask[i].size(2) - length).fill_(1)
+        x = x.masked_fill(mask, 0)
+        return x
+
+
+class MaskCNN(nn.Module):
+    def forward(self, x, percents):
+        lengths = torch.mul(x.size(2), percents).int()
+        mask = torch.BoolTensor(x.size()).fill_(0)
+        if x.is_cuda:
+            mask = mask.cuda()
+        for i, length in enumerate(lengths):
+            length = length.item()
+            if (mask[i].size(1) - length) > 0:
+                mask[i].narrow(
+                    1, length, mask[i].size(1) - length).fill_(1)
+        x = x.masked_fill(mask, 0)
+        return x
+
+
+if __name__ == "__main__":
+    input = torch.rand([8, 1, 64, 512], dtype=torch.float32)
+    percents = torch.rand([8], dtype=torch.float32)
+    model = MyModel2([' ', 'a'])
+    out = model(input, percents)
+    print(out.size())
--- train.py
+++ train.py
@@ -4,14 +4,16 @@ from exp_loggers import init_loggers
 import torch
 import pytorch_lightning as pl
 import torchvision
-from torchvision import transforms
-from torchvision.datasets import MNIST
+from data_module import LibriDataModule
 from torch.utils.data import DataLoader
 import pytorch_lightning.metrics as metrics
 import os
 import hydra
 from omegaconf import DictConfig, OmegaConf
 from utils import get_mnist_pair
+from models.QuartNet import MyModel2
+from decoder import GreedyDecoder
+from utils.asr_metrics import WER
 import logging
 logger = logging.getLogger(__name__)
 
@@ -22,13 +24,10 @@ class LightingModule(pl.LightningModule):
         """
         用于推理阶段
 
-        :param x: 传入参数 N*1*28*28
+        :param x: 传入参数 N*1*64×L
         :return: lists 预测的原始值
         """
-        e = self.encoder(x)  # N*16*28*28
-        e_view = e.view(e.size(0), -1)  # N*(16*28*28)
-        d = self.decoder(e_view)  # N*10
-        return d
+        return self.encoder(x)  # N*L'*C
 
     def configure_optimizers(self):
         """
@@ -53,30 +52,43 @@ class LightingModule(pl.LightningModule):
         :param batch_idx: index
         :return: loss: torch.Tensor, 或者一个字典, 但必须包含'loss' key
         """
-        x, y = batch
-        d = self.forward(x)  # N*10
-        loss = self.loss(d, y)
+        input = batch[0]
+        percents = batch[2]
+        trans = batch[1]
+        trans_lengths = batch[3]
+        out = self.encoder(input, percents)
+        t_lengths = torch.mul(out.size(1), percents).int()  # 输出实际长度
+        loss = self.loss(out.transpose(0, 1),
+                         trans, t_lengths, trans_lengths)
         self.log('train_loss', loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
-        self.log('train_acc', self.acc(d, y),
+        self.log('train_wer', self.wer(out.argmax(dim=-1, keepdim=False), trans, trans_lengths),
                  on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        if batch_idx%50 == 0:
+            logging.info(f"\n")
+            logging.info("pred:"+str(self.decoder.decode(out)[0][0]))
+            logging.info("true:"+str(self.decoder.convert_to_strings(trans, remove_repetitions=False)[0]))
         return loss
 
     def validation_step(self, batch, batch_idx):
-        x, y = batch
-        d = self.forward(x)  # N*10
-        predict, true = get_mnist_pair(d, y)
-        val_loss = self.loss(d, y)
-        self.log('val_acc', self.acc(d, y),
+        input = batch[0]
+        percents = batch[2]
+        trans = batch[1]
+        trans_lengths = batch[3]
+        out = self.encoder(input, percents)
+        t_lengths = torch.mul(out.size(1), percents).int()  # 输出实际长度
+        loss = self.loss(out.transpose(0, 1),
+                         trans, t_lengths, trans_lengths)
+        self.log('val_wer', self.wer(out.argmax(dim=-1, keepdim=False), trans, trans_lengths),
                  on_epoch=True, prog_bar=True, logger=True)
-        self.log('val_loss', val_loss,
-                 on_epoch=True, prog_bar=False, logger=True)
-        return {
-            'val_loss': val_loss,
-            'images': batch[0],
-            'val_acc': self.acc(d, y),
-            'pred': predict,
-            'true': true
+        self.log('val_loss', loss, on_epoch=True, prog_bar=True, logger=True)
+        return_val = {
+            'val_loss': loss,
+            'input': batch[0],
+            'val_wer': self.wer(out.argmax(dim=-1, keepdim=False), trans, trans_lengths),
+            'pred': self.decoder.decode(out),
+            'true': self.decoder.convert_to_strings(trans, remove_repetitions=False)
         }
+        return return_val
 
     def validation_epoch_end(self, outputs: List[Any]) -> None:
         """
@@ -85,13 +97,14 @@ class LightingModule(pl.LightningModule):
         :param outputs: 每一step的结果的列表
         :return: None
         """
+        pass
         # 找出一轮中准确率最低的batch的前8个
-        min_sample = min(outputs, key=lambda x: x['val_acc'])
-        grid = torchvision.utils.make_grid(min_sample['images'][0:8], nrow=4)
-        transform = torchvision.transforms.ToPILImage()
-        image = transform(grid)
-        pred = min_sample['pred'][0:8]
-        true = min_sample['true'][0:8]
+        # min_sample = min(outputs, key=lambda x: x['val_acc'])
+        # grid = torchvision.utils.make_grid(min_sample['images'][0:8], nrow=4)
+        # transform = torchvision.transforms.ToPILImage()
+        # image = transform(grid)
+        # pred = min_sample['pred'][0:8]
+        # true = min_sample['true'][0:8]
         # 使用comet的logger
         # self.logger[1].experiment.log_image(image_data=image, name=str(pred)+str(true)+'.png')
 
@@ -111,23 +124,16 @@ class LightingModule(pl.LightningModule):
         # logger.info(checkpoint.keys())
         pass
 
-    def __init__(self, learning_rate=5e-3, weight_decay=1e-4):
+    def __init__(self, learning_rate=5e-3, weight_decay=1e-4, labels=None):
         super().__init__()
-        self.acc = metrics.Accuracy()
         self.learning_rate = learning_rate
         self.weight_decay = weight_decay
+        self.labels = labels
         self.save_hyperparameters()
-        self.loss = torch.nn.CrossEntropyLoss()
-        self.softmax = torch.nn.Softmax(dim=-1)
-        self.encoder = torch.nn.Sequential(
-            torch.nn.Conv2d(1, 16, 3, stride=1, padding=1),
-            torch.nn.BatchNorm2d(16),
-            torch.nn.ReLU()
-        )
-        self.decoder = torch.nn.Sequential(
-            torch.nn.Linear(28 * 28 * 16, 10),
-            torch.nn.ReLU()
-        )
+        self.wer = WER(vocabulary=self.labels)
+        self.loss = torch.nn.CTCLoss(blank=len(self.labels))  # 最后一个作为black
+        self.encoder = MyModel2(labels=self.labels)
+        self.decoder = GreedyDecoder(labels=self.labels)
 
 
 @hydra.main(config_path='conf', config_name='conf')
@@ -143,35 +149,27 @@ def main(cfg: DictConfig):
                                                        filename="mnist-{epoch:02d}-{val_loss:.2f}")
     lr_callback = pl.callbacks.LearningRateMonitor(logging_interval='step')
     loggers = init_loggers(cfg=logger_cfg)
-    dataset = MNIST(data_cfg.get('path'), download=True, transform=transforms.ToTensor())
-    mnist_test = MNIST(data_cfg.get('path'), train=False, download=True, transform=transforms.ToTensor())
-    train_loader = DataLoader(dataset, batch_size=tran_cfg.get("train_batch_size"), num_workers=6)
-    test_loader = DataLoader(mnist_test, batch_size=128, shuffle=False, num_workers=6)
+    data_module = LibriDataModule(data_cfg.get('train_manifest'), data_cfg.get('val_manifest'),
+                                  labels=data_cfg.get('labels'), train_bs=tran_cfg.get('train_batch_size'),
+                                  dev_bs=tran_cfg.get('dev_batch_size'))
     model = LightingModule(learning_rate=tran_cfg.get("learning_rate"),
-                           weight_decay=tran_cfg.get("weight_decay"))
-    if tran_cfg.get('use_tpu'):  # 使用googleTPU训练
-        trainer = pl.Trainer(tpu_cores=tran_cfg.get('tpu_core_num'),
-                             logger=loggers,
-                             callbacks=[checkpoint_callback, lr_callback],
-                             resume_from_checkpoint=tran_cfg.get('checkpoint'),
-                             # auto_lr_find=True,
-                             amp_level=tran_cfg.get('amp_level'),
-                             precision=tran_cfg.get('precision'),
-                             amp_backend=tran_cfg.get('amp_backend'),
-                             profiler="simple",
-                             max_epochs=tran_cfg.get('total_epoch'))
-    else:
-        trainer = pl.Trainer(gpus=tran_cfg.get('gpus'),
-                             logger=loggers,
-                             callbacks=[checkpoint_callback, lr_callback],
-                             resume_from_checkpoint=tran_cfg.get('checkpoint'),
-                             # auto_lr_find=True,
-                             amp_level=tran_cfg.get('amp_level'),
-                             precision=tran_cfg.get('precision'),
-                             amp_backend=tran_cfg.get('amp_backend'),
-                             profiler="simple",
-                             max_epochs=tran_cfg.get('total_epoch'))
-    trainer.fit(model, train_loader, test_loader)
+                           weight_decay=tran_cfg.get("weight_decay"),
+                           labels=data_cfg.get('labels'))
+    trainer = pl.Trainer(gpus=tran_cfg.get('gpus'),
+                            tpu_cores=tran_cfg.get('tpu_core_num'),  # google tpu训练
+                            logger=loggers,
+                            callbacks=[checkpoint_callback, lr_callback],
+                            resume_from_checkpoint=tran_cfg.get('checkpoint'),
+                            # auto_lr_find=True,
+                            accelerator=tran_cfg.get("accelerator"),  # ddp
+                            amp_level=tran_cfg.get('amp_level'),  # 'O1', 'O2'
+                            precision=tran_cfg.get('precision'),  # 16 or 32
+                            amp_backend=tran_cfg.get('amp_backend'),  # native推荐
+                            profiler="simple",  # 打印各个函数执行时间
+                            accumulate_grad_batches=1,  # 提高batch_size的办法
+                            # limit_val_batches=0.02,
+                            max_epochs=tran_cfg.get('total_epoch'))
+    trainer.fit(model, datamodule=data_module)
 
 
 if __name__ == '__main__':
